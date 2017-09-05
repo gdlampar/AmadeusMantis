68e2d0542b0dbe123a37b9ca75375f94b29be5ae
Amadeus Pardo <amadeus.pardo@flex.com>
2017-09-05 11:10:06 -0700

    [Studio] Add screen "issuelist"

--- client/states.js
+++ client/states.js
@@ -51,5 +51,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.issuelist', {
+        views: {
+            app: {
+                controller: 'app_issuelist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.issuelist');
+                }
+            }
+        }
     });
 });
